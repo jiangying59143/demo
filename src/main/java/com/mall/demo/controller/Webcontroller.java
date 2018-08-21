5a99ef250fc128bac80be42b26267fe3fe07b2e9
jiangying59143 <907292671@qq.com>
2018-08-21 18:09:05 +0800

    shiro

--- src/main/java/com/mall/demo/controller/Webcontroller.java
+++ src/main/java/com/mall/demo/controller/Webcontroller.java
@@ -2,14 +2,16 @@ package com.mall.demo.controller;
 
 import com.mall.demo.dao.UserRepository;
 import com.mall.demo.model.User;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * 1) 如果只是使用@RestController注解Controller，则Controller中的方法无法返回jsp页面，或者html，
@@ -24,10 +26,49 @@ public class Webcontroller {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/")
-    public String test(Model model){
-        model.addAttribute("user", new User());
-        return "test";
+//    @RequestMapping("/")
+//    public String test(Model model){
+//        model.addAttribute("user", new User());
+//        return "test";
+//    }
+
+    @RequestMapping({"/","/index"})
+    public String index(){
+        return"/index";
+    }
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, Map<String, Object> map) throws Exception{
+        System.out.println("HomeController.login()");
+        // 登录失败从request中获取shiro处理的异常信息。
+        // shiroLoginFailure:就是shiro异常类的全类名.
+        String exception = (String) request.getAttribute("shiroLoginFailure");
+        System.out.println("exception=" + exception);
+        String msg = "";
+        if (exception != null) {
+            if (UnknownAccountException.class.getName().equals(exception)) {
+                System.out.println("UnknownAccountException -- > 账号不存在：");
+                msg = "UnknownAccountException -- > 账号不存在：";
+            } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
+                System.out.println("IncorrectCredentialsException -- > 密码不正确：");
+                msg = "IncorrectCredentialsException -- > 密码不正确：";
+            } else if ("kaptchaValidateFailed".equals(exception)) {
+                System.out.println("kaptchaValidateFailed -- > 验证码错误");
+                msg = "kaptchaValidateFailed -- > 验证码错误";
+            } else {
+                msg = "else >> "+exception;
+                System.out.println("else -- >" + exception);
+            }
+        }
+        map.put("msg", msg);
+        // 此方法不处理登录成功,由shiro进行处理
+        return "/login";
+    }
+
+    @RequestMapping("/403")
+    public String unauthorizedRole(){
+        System.out.println("------没有权限-------");
+        return "403";
     }
 
     @RequestMapping(value = "/user/save", method = RequestMethod.POST)
